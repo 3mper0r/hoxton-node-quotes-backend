f6dd0c910429308a1c2e91ba79201cf04c0aeaab
3mper0r <83674499+3mper0r@users.noreply.github.com>
2022-02-15 10:03:33 +0100

    form

--- quotes.ts
+++ quotes.ts
@@ -30,12 +30,33 @@ const qouteOftheweek: Quote[] = [
     }
 ]
 
+const addQuoteForm = `<form action="">
+<label htmlFor="name">Name:</label>
+<input type="text" name="name" placeholder="Name" />
+<label htmlFor="lastname">Last Name:</label>
+<input type="text" name="lastname" placeholder="Last Name" />
+<label htmlFor="quote">Quote:</label>
+<textarea name="quote" id="" cols={20} rows={5}></textarea>
+<label htmlFor="year">Year:</label>
+<input type="number" name="year" placeholder="Year" />
+<label htmlFor="bc">BC</label>
+<input type="radio" name="bcac" />
+<label htmlFor="ac">AC</label>
+<input type="radio" name="bcac" />
+<input id="submit" type="submit" onSubmit={(e) => e.preventDefault} />
+</form>`
+
 app.get('/quotes', (req, res) => {
     res.send(quotes)
 })
 
 app.get('/quoteoftheweek', (req, res) => {
     res.send(qouteOftheweek)
+})
+
+app.get('/add-quote', (req, res) => {
+
+    res.send(addQuoteForm)
 })
 
 app.listen(port, () => console.log('server is running on port 3001')
